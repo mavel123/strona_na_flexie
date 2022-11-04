bc3f97b7f63d9cc4993b4669e14d4806e68fe91d
Allah <mail@maial.pl>
2022-11-04 09:08:30 +0100

    half part of site done

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const nav = document.querySelector('.nav')
+const navBtn = document.querySelector('.burger-btn')
+const allNavItems = document.querySelectorAll('.nav__item')
+
+
+const handleNavAnimation = () => {
+    let delayTime=0;
+    allNavItems.forEach(item => {
+        item.classList.toggle('nav-items-animation')
+        item.style.animationDelay='.' + delayTime + 's';
+        delayTime++;
+    })
+}
+
+const handleNav = () => {
+	nav.classList.toggle('nav--active')
+
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active')
+		})
+	})
+
+	handleNavAnimation();
+}
+
+navBtn.addEventListener('click', handleNav)
